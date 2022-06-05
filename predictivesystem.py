e630164c93a2f6f32ca4de4b8e26a9be7be29c6d
PavanChikkodikar <pavanchikkodikar@gmail.com>
2022-06-05 17:25:47 +0530

    commit

--- predictivesystem.py
+++ predictivesystem.py
@@ -1,5 +1,4 @@
 
-
 import numpy as np
 import pickle
 
@@ -9,14 +8,18 @@ loaded_model = pickle.load(open('C:/Users/pavan/Desktop/ML and DL/ML Projects/Di
 
 input_data = (5,166,72,19,175,25.8,0.587,51)
 
+
 # changing the input_data to numpy array
 input_data_as_numpy_array = np.asarray(input_data)
+
 
 # reshape the array as we are predicting for one instance
 input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
+
 prediction = loaded_model.predict(input_data_reshaped)
 print(prediction)
+
 
 if (prediction[0] == 0):
   print('The person is not diabetic')
